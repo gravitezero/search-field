4e6ef9e790531f252ec439d05a25d80c5de0324f
Etienne Brodu <etienne.brodu@insa-lyon.fr>
2012-11-08 17:44:23 +0000

    adjustments

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
 	$(document).find('.field').each(function(i, field) {
-		$(field).find('h2').click(function(){
-			console.log('expand');
-			$(field).addClass('expanded');
-		});
-		
-		$(field).find('.close').click(function(){
-			console.log('collapse');
-			$(field).removeClass('expanded');
-		});
-	});
 
+		if (i === 0) {
+			var input = $(field).find('input');
+			$(field).find('.close').click(function() {
+				$(input).val('');
+			})
+		} else {
+			$(field).find('h2').click(function(){
+				console.log('expand');
+				$(field).addClass('expanded');
+			});
+			
+			$(field).find('.close').click(function(){
+				console.log('collapse');
+				$(field).removeClass('expanded');
+			});
+		}
+	});
 })
